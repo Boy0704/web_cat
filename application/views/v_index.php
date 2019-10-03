58d84db45412780f1e44c927569f7bc224ad2db4
BOY KURNIAWAN <boykurniawan123@gmail.com>
2019-10-03 13:20:27 +0700

    update notice

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -97,11 +97,11 @@
     $(document).ready(function() {
       var unique_id = $.gritter.add({
         // (string | mandatory) the heading of the notification
-        title: 'Welcome to Dashio!',
+        title: 'Selamat Datang Administrator!',
         // (string | mandatory) the text inside the notification
-        text: 'Hover me to enable the Close Button. You can hide the left sidebar clicking on the button next to the logo.',
+        text: '.',
         // (string | optional) the image to display on the left
-        image: 'img/ui-sam.jpg',
+        image: 'assets/img/ui-sam.jpg',
         // (bool | optional) if you want it to fade out on its own or just sit there
         sticky: false,
         // (int | optional) the time you want it to be alive for before fading out
